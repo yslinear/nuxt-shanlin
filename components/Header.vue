83f8250daf3c911ea7ac130e8f76e880e3ab7a34
Ying-Shan Lin <yslinear@gmail.com>
2023-07-01 22:18:18 +0800

    修正 Header

--- components/Header.vue
+++ components/Header.vue
@@ -20,8 +20,8 @@
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li v-for="(link, index) in links" :key="index">
-            <a :href="link.href" class="block py-2 pl-3 pr-4 md:p-0 text-white font-light"
-              :class="{ 'font-bold': $route.path === link.href }" aria-current="page">{{ link.text }}</a>
+            <a :href="link.href" class="block py-2 pl-3 pr-4 md:p-0 text-white"
+              :class="`${$route.path === link.href ? 'font-bold': 'font-light'}`" aria-current="page">{{ link.text }}</a>
           </li>
         </ul>
       </div>
