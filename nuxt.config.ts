47b6c75f6311591cfcd784b804a5daa5a4d4dee8
Ying-Shan Lin <yslinear@gmail.com>
2023-07-01 14:18:18 +0800

    安裝 `nuxtjs/tailwindcss`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-headlessui"],
+  headlessui: {
+    prefix: "Headless",
+  },
   pages: true,
+  devtools: { enabled: true },
 });
