214f9014508e99198666c882108e7f5fee2de57b
Ying-Shan Lin <yslinear@gmail.com>
2023-07-02 16:32:45 +0800

    `articles` 頁面顯示圖片

--- pages/articles.vue
+++ pages/articles.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap justify-center gap-4 py-8">
       <ul>
         <li v-for="article in articles" :key="article.id">
-          <img src="https://fakeimg.pl/300x250/" class="w-64 h-64 object-cover rounded-md mb-4 shadow-lg">
+          <img :src="`http://localhost:1337${article.attributes.photos.data[0].attributes.media.data.attributes.url}`" class="w-64 h-64 object-cover rounded-md mb-4 shadow-lg">
           <div class="text-sm mb-4">{{ article.attributes.createdAt }}</div>
           <div class="text-base font-bold mb-4">{{ article.attributes.title }}</div>
           <p class="text-sm">{{ article.attributes.content }}</p>
@@ -19,8 +19,9 @@ const { find } = useStrapi4()
 const { locale } = useI18n()
 
 const articles = ref([])
+
 onBeforeMount(async () => {
-  const response = await find('articles', { populate: 'media,createdBy', locale: locale.value })
+  const response = await find('articles', { populate: 'photos.media,createdBy', locale: locale.value })
   articles.value = response.data
 })
 </script>
