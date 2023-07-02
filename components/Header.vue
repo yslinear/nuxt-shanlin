96355b2d3204e761143e643d4d1d377f46ad7146
Ying-Shan Lin <yslinear@gmail.com>
2023-07-02 23:17:46 +0800

    調整 Footer, Header 以及其 icon

--- components/Header.vue
+++ components/Header.vue
@@ -5,23 +5,12 @@
         <img src="https://www.shanlin.co/templates/g5_helium/custom/images/logo-no-background.png" class="h-8 mr-3"
           alt="ShanLin Logo" />
       </a>
-      <button data-collapse-toggle="navbar-default" type="button"
-        class="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden"
-        aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clip-rule="evenodd"></path>
-        </svg>
-      </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div>
         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0">
           <li v-for="(link, index) in links" :key="index">
-            <a :href="link.href" class="block py-2 pl-3 pr-4 md:p-0 text-slate-800"
-              :class="`${$route.path === link.href ? 'font-bold' : 'font-light'}`" aria-current="page"
-              v-t="link.text"></a>
+            <a :href="link.href" class="block py-2 pl-3 pr-4 md:p-0 text-slate-800">
+              <Icon :name="`${$route.path.includes(link.href) ? `${link.icon}-fill` : link.icon}`" class="text-2xl" />
+            </a>
           </li>
         </ul>
       </div>
@@ -34,9 +23,8 @@ export default {
   data() {
     return {
       links: [
-        { text: 'home', href: '/' },
-        { text: 'articles', href: '/articles' },
-        { text: 'photos', href: '/photos' }
+        { text: 'articles', href: '/articles', icon: 'ph:article' },
+        { text: 'photos', href: '/photos', icon: 'ph:image-square' }
       ]
     };
   }
